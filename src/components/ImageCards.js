2759343fbab3a99df8df6d262b2593a5746622aa
programmerNizam <mdnizam12109@gmail.com>
2022-12-18 14:18:45 +0600

    image gallery project completed

--- src/components/ImageCards.js
+++ src/components/ImageCards.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Card from './Card';
+import Loader from './Loader';
 
-const ImageCards = ({ images }) => {
+const ImageCards = ({ images, loading }) => {
 
     return (
         <div className='my-6'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+            {!loading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+            {loading ? <Loader /> : <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
                 {images.map(image => <Card key={image.id} image={image} />)}
-            </div>
+            </div>}
         </div>
     );
 };
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div>
-            <svg className="animate-spin h-10 w-10 mr-3 outline-dotted rounded-full outline-8 outline-purple-500" viewBox="0 0 64 64">
+        <div className='w-100 flex justify-center'>
+            <svg className="animate-spin h-10 w-10 mr-3 outline-dotted rounded-full outline-8 outline-purple-500 duration-700" viewBox="0 0 64 64">
             </svg>
         </div>
     );
